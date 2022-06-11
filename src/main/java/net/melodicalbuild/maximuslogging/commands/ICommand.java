702de876a42a4f5603f8a86eb3f91112abd2de81
MelodicAlbuild <ralexdrum@gmail.com>
2022-06-10 22:43:53 -0500

    Version 1.1-Pre1
    Signed-off-by: MelodicAlbuild <ralexdrum@gmail.com>

---
+++ src/main/java/net/melodicalbuild/maximuslogging/commands/ICommand.java
@@ -0,0 +1,16 @@
+package net.melodicalbuild.maximuslogging.commands;
+
+import net.melodicalbuild.maximuslogging.MaximusLogging;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public interface ICommand {
+
+    void run(@NotNull final MaximusLogging pl, @NotNull final CommandSender sender, @NotNull final String[] args);
+
+    @NotNull
+    List<String> getTabCompletition(@NotNull final String[] args);
+
+}
